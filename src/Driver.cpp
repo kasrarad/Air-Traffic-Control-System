52adcd94655f279014e0e0291ae5d7637ad80772
DPigeon <D_Pig@encs.concordia.ca>
2019-10-03 02:30:23 -0400

    Initializing with the main function

---
+++ src/Driver.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+using namespace std;
+
+/* Global Variables */
+// Convention: km/h
+double elevationDistance = 0.3048; // 1000 feet
+double collisionDistance = 4.82803; // 3 miles
+double aircraftDimension[3] = {30.48, 7.62, 4.572}; // Converted to km (100 000 feet, 25 000 feet, 15 000 feet)
+
+int main() {
+	cout<<"Test";
+
+	return 0;
+}
